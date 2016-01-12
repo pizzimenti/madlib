a64a9e37df3d47802043676cb59b91cb88bbab09
Brad Pizzimenti and Steve Kinney <brad@gennakersystems.com>
2016-01-12 14:31:23 -0800

    complete html structure, append info for story

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#blanks' form).submit(function(event) {
+    $('.person1').append("TEST");
+    $('.person2').append("TEST");
+    $('.insult').append("TEST");
+    $('.weather').append("TEST");
+    $('.bodypart').append("TEST");
+    $('.toolname').append("TEST");
+    $('.activity').append("TEST");
+
+    $('#story').show();
+    
+
+  }
+
+}
