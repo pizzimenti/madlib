9e13db3d12a8d3180f29dd3e4c1af5763a44b04b
Brad Pizzimenti and Steve Kinney <skinney103@gmail.com>
2016-01-12 15:00:44 -0800

    link form input to js variables

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-  $('#blanks' form).submit(function(event) {
+  $('#blanks form').submit(function(event) {
+var person1Input = $("input#person1").val();
+var person2Input = $("input#person2").val();
+var insultInput = $("input#insult").val();
+var weatherInput = $("input#weather").val();
+var bodypartInput = $("input#bodypart").val();
+var toolnameInput = $("input#toolname").val();
+var activityInput = $("input#activity").val();
+
+
     $('.person1').append("TEST");
     $('.person2').append("TEST");
     $('.insult').append("TEST");
@@ -9,8 +18,7 @@ $(document).ready(function() {
     $('.activity').append("TEST");
 
     $('#story').show();
-    
 
-  }
-
-}
+    event.preventDefault();
+  });
+});
